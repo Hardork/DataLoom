309ebaa45f486c2848e7316c341a561bf96d0432
Abomination <2502022483@qq.com>
2024-08-23 09:28:33 +0800

    添加识别原始字段类型功能

---
+++ admin-backend/src/main/java/com/hwq/dataloom/service/impl/CoreDatasetTableFieldServiceImpl.java
@@ -0,0 +1,22 @@
+package com.hwq.dataloom.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hwq.dataloom.mapper.CoreDatasetTableFieldMapper;
+import com.hwq.dataloom.model.entity.CoreDatasetTableField;
+import com.hwq.dataloom.service.CoreDatasetTableFieldService;
+import org.springframework.stereotype.Service;
+
+/**
+* @author 25020
+* @description 针对表【core_dataset_table_field(table数据集表字段)】的数据库操作Service实现
+* @createDate 2024-08-21 17:22:45
+*/
+@Service
+public class CoreDatasetTableFieldServiceImpl extends ServiceImpl<CoreDatasetTableFieldMapper, CoreDatasetTableField>
+    implements CoreDatasetTableFieldService {
+
+}
+
+
+
+
