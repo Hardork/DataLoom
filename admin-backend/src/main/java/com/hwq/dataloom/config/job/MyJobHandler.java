0b02af7f96da779d0a22e4147a3b96c6e655344f
Abomination <2502022483@qq.com>
2024-08-29 15:06:07 +0800

    feat: 新增定时任务会根据updateType实现全量和增量更新

--- admin-backend/src/main/java/com/hwq/dataloom/config/job/MyJobHandler.java
+++ admin-backend/src/main/java/com/hwq/dataloom/config/job/MyJobHandler.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.net.HttpHeaders;
+import com.hwq.dataloom.constant.DatasourceConstant;
 import com.hwq.dataloom.framework.errorcode.ErrorCode;
 import com.hwq.dataloom.framework.exception.BusinessException;
 import com.hwq.dataloom.framework.exception.ThrowUtils;
@@ -15,11 +16,13 @@ import com.hwq.dataloom.model.dto.newdatasource.TableField;
 import com.hwq.dataloom.model.entity.CoreDatasetTable;
 import com.hwq.dataloom.model.entity.CoreDatasetTableField;
 import com.hwq.dataloom.model.entity.CoreDatasourceTask;
+import com.hwq.dataloom.model.enums.DataSourceTypeEnum;
 import com.hwq.dataloom.service.CoreDatasetTableFieldService;
 import com.hwq.dataloom.service.CoreDatasetTableService;
 import com.hwq.dataloom.service.CoreDatasourceService;
 import com.hwq.dataloom.service.CoreDatasourceTaskService;
 import com.hwq.dataloom.utils.ApiUtils;
+import com.hwq.dataloom.utils.datasource.DatasourceEngine;
 import com.xxl.job.core.biz.AdminBiz;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
@@ -41,6 +44,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.*;
 
 @Component
@@ -58,6 +62,9 @@ public class MyJobHandler {
 
     @Resource
     private CoreDatasetTableFieldService coreDatasetTableFieldService;
+
+    @Resource
+    private DatasourceEngine datasourceEngine;
 
     @Value("${xxl.job.admin.addresses:''}")
     private String adminAddresses;
@@ -84,6 +91,10 @@ public class MyJobHandler {
         CoreDatasetTable datasetTable = coreDatasetTableService.getOne(queryWrapper);
         ThrowUtils.throwIf(ObjectUtils.isEmpty(datasetTable), ErrorCode.PARAMS_ERROR, "数据表不存在");
         Long datasetTableId = datasetTable.getId();
+        Long datasourceId = datasetTable.getDatasourceId();
+        QueryWrapper<CoreDatasourceTask> taskQueryWrapper = new QueryWrapper<>();
+        taskQueryWrapper.eq("datasetTableId", datasetTableId);
+        CoreDatasourceTask coreDatasourceTask = coreDatasourceTaskService.getOne(taskQueryWrapper);
         try {
             CloseableHttpResponse response = ApiUtils.getApiResponse(apiDefinition);
             int code = response.getCode();
@@ -99,8 +110,10 @@ public class MyJobHandler {
             System.out.println(apiDefinition);
             // 执行并将字段更新到数据库中
             List<TableField> fields = apiDefinition.getFields();
+            TableField uniqueField = new TableField();
             int columnIndex = 0;
-            ArrayList<CoreDatasetTableField> coreDatasetTableFieldList = new ArrayList<>();
+            List<CoreDatasetTableField> coreDatasetTableFieldList = new ArrayList<>();
+            String[] columns = new String[fields.size()];
             for (TableField field : fields) {
                 columnIndex++;
                 CoreDatasetTableField coreDatasetTableField = new CoreDatasetTableField();
@@ -110,21 +123,57 @@ public class MyJobHandler {
                 coreDatasetTableField.setLastSyncTime(now.getTime());
                 coreDatasetTableField.setGroupType("d");
                 coreDatasetTableFieldList.add(coreDatasetTableField);
+                // 获取唯一字段
+                if (field.getIsUnique() == 1) {
+                    uniqueField = field;
+                }
+                columns[columnIndex - 1] = field.getOriginName();
             }
             boolean savedBatch = coreDatasetTableFieldService.saveOrUpdateBatch(coreDatasetTableFieldList);
             ThrowUtils.throwIf(!savedBatch,ErrorCode.OPERATION_ERROR,"新增字段失败！");
+            // TODO 更新数据仓库
+            String updateType = coreDatasourceTask.getUpdateType();
+            if (updateType.equals("all_scope")) {
+                // 全量更新
+                log.info("执行全量更新" + String.format(DatasourceConstant.TABLE_NAME_TEMPLATE, DataSourceTypeEnum.API.getValue(), datasourceId, apiDefinition.getName()));
+                datasourceEngine.exeDropTable(datasourceId,String.format(DatasourceConstant.TABLE_NAME_TEMPLATE, DataSourceTypeEnum.API.getValue(), datasourceId, apiDefinition.getName()));
+                datasourceEngine.exeCreateTable(datasourceId,String.format(DatasourceConstant.TABLE_NAME_TEMPLATE, DataSourceTypeEnum.API.getValue(), datasourceId, apiDefinition.getName()),coreDatasetTableFieldList);
+                List<String[]> dataList = ApiUtils.toDataList(JSONUtil.toJsonStr(apiDefinition));
+                int pageNumber = 1000; //一次插入 1000条
+                int totalPage;
+                if (dataList.size() % pageNumber > 0) {
+                    totalPage = dataList.size() / pageNumber + 1;
+                } else {
+                    totalPage = dataList.size() / pageNumber;
+                }
+                for (int page = 1; page <= totalPage; page++) {
+                    datasourceEngine.execInsert(datasourceId, String.format(DatasourceConstant.TABLE_NAME_TEMPLATE, DataSourceTypeEnum.API.getValue(), datasourceId, apiDefinition.getName()), dataList, page, pageNumber);
+                }
+            } else {
+                // 增量更新
+                log.info("执行增量更新" + String.format(DatasourceConstant.TABLE_NAME_TEMPLATE, DataSourceTypeEnum.API.getValue(), datasourceId, apiDefinition.getName()));
+                ThrowUtils.throwIf(ObjectUtils.isEmpty(uniqueField),ErrorCode.PARAMS_ERROR,"执行增量更新失败，未设置唯一字段！");
+                // 获取增量数据
+                List<String[]> dataList = ApiUtils.toDataList(JSONUtil.toJsonStr(apiDefinition));
+                int pageNumber = 1000; //一次插入 1000条
+                int totalPage;
+                if (dataList.size() % pageNumber > 0) {
+                    totalPage = dataList.size() / pageNumber + 1;
+                } else {
+                    totalPage = dataList.size() / pageNumber;
+                }
+                for (int page = 1; page <= totalPage; page++) {
+                    // 添加增量数据进入数据仓库
+                    datasourceEngine.execInsertAndUpdate(datasourceId,String.format(DatasourceConstant.TABLE_NAME_TEMPLATE, DataSourceTypeEnum.API.getValue(), datasourceId, apiDefinition.getName()),dataList,page,pageNumber,columns,uniqueField);
+                }
+            }
         } catch (Exception e) {
             // 更新任务信息
-            QueryWrapper<CoreDatasourceTask> taskQueryWrapper = new QueryWrapper<>();
-            taskQueryWrapper.eq("datasetTableId", datasetTableId);
-            CoreDatasourceTask coreDatasourceTask = coreDatasourceTaskService.getOne(taskQueryWrapper);            coreDatasourceTask.setLastExecTime(now.getTime());
+            coreDatasourceTask.setLastExecTime(now.getTime());
             coreDatasourceTask.setLastExecStatus("failed");
             coreDatasourceTaskService.updateById(coreDatasourceTask);
         }
         // 更新任务信息
-        QueryWrapper<CoreDatasourceTask> taskQueryWrapper = new QueryWrapper<>();
-        taskQueryWrapper.eq("datasetTableId", datasetTableId);
-        CoreDatasourceTask coreDatasourceTask = coreDatasourceTaskService.getOne(taskQueryWrapper);
         coreDatasourceTask.setLastExecTime(now.getTime());
         coreDatasourceTask.setLastExecStatus("succeed");
         coreDatasourceTaskService.updateById(coreDatasourceTask);
