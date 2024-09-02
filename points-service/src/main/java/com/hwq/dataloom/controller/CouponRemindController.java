fe45983049453ee5c7fd708c7371981c84af4d98
hwq <1602370456@qq.com>
2024-09-03 02:20:36 +0800

    feat(points-service): 优惠券预约功能TODO

---
+++ points-service/src/main/java/com/hwq/dataloom/controller/CouponRemindController.java
@@ -0,0 +1,17 @@
+package com.hwq.dataloom.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HWQ
+ * @date 2024/9/3 02:13
+ * @description 优惠券预约接口
+ */
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "优惠券预约提醒管理")
+public class CouponRemindController {
+    // TODO：优惠券预约
+}
